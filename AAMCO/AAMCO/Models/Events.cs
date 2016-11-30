d191d020c6f0737d8cc78f4680406ae900da2ca2
LoshaAge <alexey.moshe@gmail.com>
2016-11-30 00:28:41 -0500

    Add Events class

---
+++ AAMCO/AAMCO/Models/Events.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace AAMCO.Models
+{
+    public class Events
+    {
+        public int id { get; set; }
+        public int id1 { get; set; }
+        public int id2 { get; set; }
+        public int id43 { get; set; }
+        public int id5 { get; set; }
+        public int id7 { get; set; }
+    
+    }
+}
