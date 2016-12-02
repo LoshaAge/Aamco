a53b85ab1475725cb62ca1c1228cc2af4cedbded
Alex <Alex@DESKTOP-2PGIJ73>
2016-12-02 05:10:17 -0800

    qwe

--- AAMCO/AAMCO/Models/Events.cs
+++ AAMCO/AAMCO/Models/Events.cs
@@ -9,11 +9,11 @@ namespace AAMCO.Models
     public class Event
     {
         public int Id { get; set; }
-        public int Year { get; set; }
+        public int YearCar { get; set; }
         public string NameCar { get; set; }
-        public int Message { get; set; }
+        public int Mess { get; set; }
         public int Name { get; set; }
-        public int SName { get; set; }
+        public int SecName { get; set; }
         public int Email { get; set; }
         public int Phone { get; set; }
     }
